05ae5adb4263bd271ba84b4a95f826e2635fd5d4
KozlArt <91756476+KozlArt@users.noreply.github.com>
2022-11-25 12:50:46 +0300

    Add files via upload

--- CV/src/make_annotation_file.py
+++ CV/src/make_annotation_file.py
@@ -1,27 +1,31 @@
 import os
 import pandas as pd
 
-print("Plese type mode(train or test)")
-mode = input(">")
-print("Please type annotation file name")
-filename = input(">")
-print("Please type basic directory")
-base_dir = input(">")
+def make():
+    print("Plese type mode(train or test)")
+    mode = input(">")
+    print("Please type annotation file name")
+    filename = input(">")
+    print("Please type basic directory")
+    base_dir = input(">")
 
-temp_data_list = []
-if(mode == "train"):
-    for path_idx, path in enumerate(os.listdir(base_dir)):
+    temp_data_list = []
+    if(mode == "train"):
+        for path_idx, path in enumerate(os.listdir(base_dir)):
 
-        temp_list = [[path + '//' + img_path, path_idx] for img_path in os.listdir(base_dir + '//' + path)]
-        temp_data_list += temp_list
+            temp_list = [[path + '//' + img_path, path_idx] for img_path in os.listdir(base_dir + '//' + path)]
+            temp_data_list += temp_list
 
-    annotations = pd.DataFrame(columns=["img_path", "target"], data=temp_data_list)
-    annotations.to_csv(f"{filename}.csv", index=False)
+        annotations = pd.DataFrame(columns=["img_path", "target"], data=temp_data_list)
+        annotations.to_csv(f"{filename}.csv", index=False)
 
-else:
+    else:
 
-    temp_data_list = [[f"{img_path}", int(img_path.split('.')[0])] for img_path in os.listdir(base_dir)]
-    annotations = pd.DataFrame(columns=["img_path", "id"], data=temp_data_list)
-    annotations.to_csv(f"{filename}.csv", index=False)
+        temp_data_list = [[f"{img_path}", int(img_path.split('.')[0])] for img_path in os.listdir(base_dir)]
+        annotations = pd.DataFrame(columns=["img_path", "id"], data=temp_data_list)
+        annotations.to_csv(f"{filename}.csv", index=False)
 
-print("=====ANNOTATION FILE CREATED=====")
+    print("=====ANNOTATION FILE CREATED=====")
+    
+if __name__ == '__main__':
+    make()
